d91d29851b1d46be97db8e54af38e96778c43deb
Ahmedchammakhii <117437634+Ahmedchammakhii@users.noreply.github.com>
2023-09-11 14:43:31 -0600

    ma skillsfinalv done

--- -myportfolio/src/components/Skills.vue
+++ -myportfolio/src/components/Skills.vue
@@ -1,11 +1,50 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
+import { onMounted, ref } from 'vue';
+import ScrollTrigger from "gsap/ScrollTrigger";
+import gsap from "gsap"
 const train = ref("<Training>")
+    onMounted(()=>{
+const timeline = gsap.timeline()
+
+timeline.to("video",{scale:10,scrollTrigger:{
+    trigger:".endoftimeline",
+    end:"bottom bottom",
+    scrub:true
+}})
+timeline.to(".right_quote",{
+    opacity:0,scrollTrigger:{
+    trigger:".endoftimeline",
+    end:"bottom bottom",
+
+    scrub:true,
+}
+},"+=5").to(".right",{
+    opacity:1,scrollTrigger:{
+    trigger:".endoftimeline",
+    end:"bottom bottom",
+
+    scrub:true,
+}
+},"+=10")
+
+    })
+    // let isTrue = ref(false)
+
+// const handleAnimationEnd = () => {
+//    const element = document.querySelector(".right_quote")
+
+//     if (element) {
+//     element.style.opacity=0;
+//     }
+//     isTrue.value = !isTrue.value ;
+//     console.log(isTrue.value);
+    
+// }
+
 </script>
 
 <template>
-<div class="content_wrapper">
+<div class="content_wrapper__skills">
     
    <div class="left">
 <h2>{{ train }}</h2>
@@ -13,7 +52,7 @@ const train = ref("<Training>")
     <h2>{{ '</' + train.slice(1)  }}</h2>
 
 </div>
-<div class="right">
+<div class="right" >
     <div class="set">
         <h2>HTML & CSS
 </h2>
@@ -40,15 +79,30 @@ const train = ref("<Training>")
         <h2>Photoshop</h2>
         <h2>After effect</h2>
     </div>
-</div></div>
+</div>
+<div class="right_quote" >
+    <h1>,,</h1>
+    <p>Majority of people can
+run a 100meter dash,
+but only a dozen can
+do it in under 9.8
+seconds</p>
+<h1>,,</h1>
+
+</div>
+<video autoplay>
+  <source src="../assets/videos/ch.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video></div>
+<div class="endoftimeline" style="height: 20vh;"></div>
 </template>
 <style scoped>
-.content_wrapper {
-    width: 80vw;
+.content_wrapper__skills {
+    width: 90vw;
     height: 60vh;
     display: flex;
 font-family: avenir;
-padding: 0 5%;
+padding-left: 5%;
 align-items: center;
 }
 .left {
@@ -58,8 +112,33 @@ align-items: center;
     display: flex;
     flex-direction: column;
     gap: 20px;
+opacity: 0;
 }
 h1 {
     font-size: calc(4rem + 1vw);
 }
+.right_quote p {
+    font-size: calc(2rem + 1vw);
+text-align: center;
+}
+.right_quote {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+.right_quote h1:nth-last-child(1){
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+}
+video {
+    width: 150px;
+    height: 100px;
+    border-radius: 50%;
+    object-fit: cover;
+    transition: all ease 5s;
+margin-left: 100px;
+}
+
 </style>
